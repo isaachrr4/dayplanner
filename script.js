5de8d6a3df3307cecc0fb5130ff1af1b478ee710
isaachrr4 <80857536+isaachrr4@users.noreply.github.com>
2021-05-30 02:46:01 -0400

    upadted planner

--- script.js
+++ script.js
@@ -1,7 +1,119 @@
-//date goes atop of the page//
-var today = moment()
-$("#currentDay").text(today.format("DD, MM, YYYY"));
+//put date on the top of the page
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do"));
 
-var day = {
+var items = {
+    "1": [],
+    "2": [],
+    "3": [],
+    "4": [],
+    "5": [],
+    "6": [],
+    "7": [],
+    "8": [],
+    "9": []
+};
 
+var setItems = function() {
+    // store items in localStorage 
+    localStorage.setItems("items", JSON.stringify(item));
 }
+
+var getItems = function() {
+    
+    var loadItems = JSON.parse(localStorage.getItem("items"));
+    if (loadItems) {
+        items = loadItems
+
+        
+        $.each(items, function(hour, items) {
+            var hourDiv = $("#" + hour);
+            createItems(items, hourDiv);
+        })
+    }
+
+
+    auditItems();
+}
+
+var createItems = function(itemsText, hourDiv) {
+    
+
+    var itemDiv = hourDiv.find(".items");
+    var itemP = $("<p>")
+        .addClass("description")
+        .text(itemsText)
+    itemDiv.html(itemsP);
+}
+
+var auditItems = function() {
+    // change the background due to time of day
+
+    var currentHour = moment().hour();
+    $(".items-info").each( function() {
+        var elementHour = parseInt($(this).attr("id"));
+
+        // handle past, present, and future
+        if ( elementHour < currentHour ) {
+            $(this).removeClass(["present", "future"]).addClass("past");
+        }
+        else if ( elementHour === currentHour ) {
+            $(this).removeClass(["past", "future"]).addClass("present");
+        }
+        else {
+            $(this).removeClass(["past", "present"]).addClass("future");
+        }
+    })
+};
+
+var replaceTextarea = function(textareaElement) {
+   
+    var itemsInfo = textareaElement.closest(".items-info");
+    var itemsArea = itemsInfo.find("textarea");
+
+   
+    var time = itemsInfo.attr("id");
+    var text = itemsArea.val().trim();
+
+    // persist information when the page is refreshed
+    item[time] = [text];  
+    setitems();
+
+ 
+    createitems(text, itemInfo);
+}
+
+$(".items").click(function() {
+
+  
+    $("textarea").each(function() {
+        replaceTextarea($(this));
+    })
+
+    var time = $(this).closest(".items-info").attr("id");
+    if (parseInt(time) >= moment().hour()) {
+
+     
+        var text = $(this).text();
+        var textInput = $("<textarea>")
+            .addClass("form-control")
+            .val(text);
+
+        
+        $(this).html(textInput);
+        textInput.trigger("focus");
+    }
+})
+
+
+
+$(".saveBtn").click(function() {
+    replaceTextarea($(this));
+})
+
+timeToHour = 3600000 - today.milliseconds();  
+setTimeout(function() {
+    setInterval(auditItems, 3600000)
+}, timeToHour);
+
+getItems();
